06c56ddabe807a3c2bd72c216d8c8ee9d423fa9f
duper <100982596+duper203@users.noreply.github.com>
2023-09-21 17:57:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if picture:
     if button:
         st.write("pushed")
         text = pytesseract.image_to_string(img)
-        print(text)
+        sr.write(text)
         if text:
             ocr_text = st.code(text, langauge='text')
             st.write(ocr_text)
