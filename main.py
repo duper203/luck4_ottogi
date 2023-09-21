d0946f237f12a8c81594967580860fec0490a41c
duper <100982596+duper203@users.noreply.github.com>
2023-09-21 17:11:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 from PIL import Image
 from io import BytesIO
 import base64
+import pytesseract
+
+data = None
 
 
 picture = st.camera_input("Take a picture")
@@ -14,7 +17,15 @@ def convert_image(img):
     return byte_im
     
 if picture:
+    #OCR
+    st.image(picture)
+    img = Image.open(picture)
+    text = pytesseract.image_to_string(img)
+
+    #다운로드 파일
     st.image(picture)
     image = Image.open(picture)
     # print(convert_image(image))
     st.download_button("Download fixed image", convert_image(image), "fixed.png", "image/png")
+if text:
+    st.code(text, langauge='text')
