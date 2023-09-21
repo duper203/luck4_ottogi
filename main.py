16b364b0bd887e0ce4a845c1057202ce526c5bed
duper <100982596+duper203@users.noreply.github.com>
2023-09-21 10:15:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ import streamlit as st
 
 picture = st.camera_input("Take a picture")
 
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+    
 if picture:
     st.image(picture)
+    image = Image.open(picture)
+    st.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
