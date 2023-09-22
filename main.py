0453be32a2ef0550a9cafe3ec62bb229962127a2
duper <100982596+duper203@users.noreply.github.com>
2023-09-22 10:06:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import streamlit as st
+from PIL import Image
+from io import BytesIO
+import base64
 import requests
 import uuid
 import time
 import json
 
-data = None
-text = None
+api_url = 'https://97c3ecilit.apigw.ntruss.com/custom/v1/24213/3330ee526497f746fcd16ae438b02b1fc334fb816c12edc24d4b448753f718c3/general'
+secret_key = 'a2d2bnVzTUVrYndHZEZ0RGxBd3pHV0ZXcXlQWlFNUVg='
 
-# Download the fixed image
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
 
-def ocr_connect(image_file):
-    api_url = st.secrets["naver_api_url"]
-    secret_key = st.secrets["naver_secret_key"]
+def ocr_connect(image_data):
+    # api_url = st.secrets["naver_api_url"]
+    # secret_key = st.secrets["naver_secret_key"]
     
     keyword_list = []
     
@@ -34,15 +31,13 @@ def ocr_connect(image_file):
     
     payload = {'message': json.dumps(request_json).encode('UTF-8')}
     files = [
-      ('file', open(image_file,'rb'))
+        ('file', image_data)
     ]
     headers = {
-      'X-OCR-SECRET': secret_key
+        'X-OCR-SECRET': secret_key
     }
     
-    response = requests.request("POST", api_url, headers=headers, data = payload, files = files)
-    
-    # print(response.text.encode('utf8'))
+    response = requests.post(api_url, headers=headers, data=payload, files=files)
     
     if response.status_code == 200:
         result = response.json()
@@ -50,38 +45,33 @@ def ocr_connect(image_file):
             for image_info in result['images']:
                 if 'fields' in image_info:
                     for field in image_info['fields']:
-                        # print(f"{field['inferText']}")
                         keyword_list.append(field['inferText'])
-        
-        
         else:
-            print("이미지 정보를 찾을 수 없습니다.")
+            st.error("이미지 정보를 찾을 수 없습니다.")
+            return
         
-        print("한장씩 keyword_list 출력완료")
-        
+        st.success("OCR 완료!")
         return keyword_list
-        
-        
     else:
-        print(f"API 요청 실패: {response.status_code}")
-        print(response.text)
+        st.error(f"API 요청 실패: {response.status_code}")
+        st.error(response.text)
+        return None
 
+st.title("OCR using Naver API")
+image_file = st.file_uploader("이미지 업로드", type=['png', 'jpg', 'jpeg'])
 
-data = st.file_uploader("upload", type=['png', 'jpg', 'jpeg'])
-
-if data:
-    ##OCR
-    image = st.image(data)
-    # img = Image.open(data)
-    button = st.button("ocr")
-   
-    if button:
-        ocr_text = ocr_connect(data)
-        st.write(ocr_text)
-
-
-
-    ##다운로드 파일
-    # st.image(picture)
-    # image = Image.open(picture)
-    # st.download_button("Download fixed image", convert_image(image), "fixed.png", "image/png")
+if image_file:
+    print("image_file:",image_file)
+    if st.button("OCR 실행"):
+        # Convert the image to PNG format
+        image = Image.open(image_file)
+        image = image.convert("JPG")
+        print("image:", image)
+        
+        
+        ocr_text = ocr_connect(image)
+        
+        if ocr_text:
+            st.write("OCR 결과:")
+            for keyword in ocr_text:
+                st.write(keyword)
